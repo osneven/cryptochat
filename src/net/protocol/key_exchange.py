384e907af2adbc2899fcda0dbdc850314c147ae2
Oliver S. Neven <oliver@neven.dk>
2016-11-24 17:34:01 +0100

    Worked on the key exchange protocol

--- src/net/protocol/key_exchange.py
+++ src/net/protocol/key_exchange.py
@@ -31,76 +31,53 @@ class KeyExchangeProtocol:
 		# Change stage sending turn
 		self._send_stage = not self._send_stage
 
+	def __encode_stage(self, stage, data=None):
+		pass
 
+	def __decode_stage(self, stage, data):
+		if not isinstance(data, bytes):
+			raise TypeError("data must be an instance of 'bytes'")
+		if not len(data) > 1:
+			raise ValueError("data must contain a packet code")
 
 	# The packet codes used to identify the packet types
 	class PacketCodes:
 		# If any error occurs, e.g a bad key, out of sync stages or an unkown packet code
 		# If this is recieved, send it back and then reset the protocol
-		ERROR									= [0x0, ERROR_method],
-		@staticmethod
-		def ERROR_method(kep):
-			return
+		ERROR									= 0x0
 
 		# Request the connection for its next request
 		# No data needed
-		REQUEST_NEXT							= [0x1, REQUEST_NEXT_method],
-		@staticmethod
-		def REQUEST_NEXT_method(kep):
-			pass
+		REQUEST_NEXT							= 0x1
 
 		# Request the connection for its public ECDH key
 		# No data needed
-		REQUEST_ECDH_PUBLIC_KEY					= [0x2, REQUEST_ECDH_PUBLIC_KEY_method],
-		@staticmethod
-		def REQUEST_ECDH_PUBLIC_KEY_method(kep):
-			pass
+		REQUEST_ECDH_PUBLIC_KEY					= 0x2
 
 		# Reply the connection's public ECDH key request
 		# Needs to be sent with the local public ECDH key
-		REPLY_ECDH_PUBLIC_KEY					= [0x3, REPLY_ECDH_PUBLIC_KEY_method],
-		@staticmethod
-		def REPLY_ECDH_PUBLIC_KEY_method(kep):
-			pass
+		REPLY_ECDH_PUBLIC_KEY					= 0x3
 
 		# Request the connection to verify its shared ECDH key
 		# Needs to be sent some random data, encrypted (AES-256) with the local shared ECDH key
-		REQUEST_ECDH_SHARED_KEY_VERIFICATION	= [0x4, REQUEST_ECDH_SHARED_KEY_VERIFICATION_method],
-		@staticmethod
-		def REQUEST_ECDH_SHARED_KEY_VERIFICATION_method(kep):
-			pass
+		REQUEST_ECDH_SHARED_KEY_VERIFICATION	= 0x4
 
 		# Reply the connection's shared ECDH verification request
 		# Needs to be sent with the digest (SHA-256) of the random decrypted data, recieved by the request
-		REPLY_ECDH_SHARED_KEY_VERIFICATION		= [0x5, REPLY_ECDH_SHARED_KEY_VERIFICATION_method],
-		@staticmethod
-		def REPLY_ECDH_SHARED_KEY_VERIFICATION_method(kep):
-			pass
+		REPLY_ECDH_SHARED_KEY_VERIFICATION		= 0x5
 
 		# Request the connection for its public RSA key, encrypted (AES-256) with the local shared ECDH key
 		# No data needed
-		REQUEST_ENCRYPTED_RSA_PUBLIC_KEY		= [0x6, REQUEST_ENCRYPTED_RSA_PUBLIC_KEY_method],
-		@staticmethod
-		def REQUEST_ENCRYPTED_RSA_PUBLIC_KEY_method(kep):
-			pass
+		REQUEST_ENCRYPTED_RSA_PUBLIC_KEY		= 0x6
 
 		# Reply the connection's encrypted public RSA request
 		# Needs to be sent with the local public RSA key, encrypted (AES-256) with the local shared ECDH key
-		REPLY_ENCRYPTED_RSA_PUBLIC_KEY			= [0x7, REPLY_ENCRYPTED_RSA_PUBLIC_KEY_method],
-		@staticmethod
-		def REPLY_ENCRYPTED_RSA_PUBLIC_KEY_method(kep):
-			pass
+		REPLY_ENCRYPTED_RSA_PUBLIC_KEY			= 0x7
 
 		# Request the connection to verify the local copy of the remote public RSA key
 		# Needs to be sent with some random data, encrypted with the local copy of the remote public RSA key
-		REQUEST_RSA_PUBLIC_KEY_VERIFICATION		= [0x8, REQUEST_RSA_PUBLIC_KEY_VERIFICATION_method],
-		@staticmethod
-		def REQUEST_RSA_PUBLIC_KEY_VERIFICATION_method(kep):
-			pass
+		REQUEST_RSA_PUBLIC_KEY_VERIFICATION		= 0x8
 
 		# Reply the connection's public RSA key verification request
 		# Needs to be sent with the digest (SHA-256) of the random decrypted data, recived by the request
-		REPLY_RSA_PUBLIC_KEY_VERIFICATION		= [0x9, REPLY_RSA_PUBLIC_KEY_VERIFICATION_method]
-		@staticmethod
-		def REPLY_RSA_PUBLIC_KEY_VERIFICATION_method(kep):
-			pass
+		REPLY_RSA_PUBLIC_KEY_VERIFICATION		= 0x9
